aee435b795746b9cc6e48b824706887b326e45f0
Marcelo Silva <mlsilva.marcelo@gmail.com>
2022-04-10 13:15:22 -0300

    feat: create resolvers

---
+++ src/http/graphql/resolvers/students.resolver.ts
@@ -0,0 +1,19 @@
+import { UseGuards } from "@nestjs/common";
+import { Query, Resolver } from "@nestjs/graphql";
+import { StudentsService } from "../../../services/students.service";
+import { AuthorizationGuard } from "../../auth/authorization.guard";
+import { Student } from "../models/student";
+
+@Resolver(() => Student)
+export class StudentResolver {
+
+    constructor(
+        private studentsService: StudentsService
+    ) { }
+
+    @UseGuards(AuthorizationGuard)
+    @Query(() => [Student])
+    students() {
+        return this.studentsService.listAll();
+    }
+}
