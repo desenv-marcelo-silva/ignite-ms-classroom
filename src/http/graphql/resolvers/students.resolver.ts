58eadcbf3b440c87383d50403960a4246ac6cb32
Marcelo Silva <mlsilva.marcelo@gmail.com>
2022-04-16 21:28:43 -0300

    feat: configure and use apollo federation

--- src/http/graphql/resolvers/students.resolver.ts
+++ src/http/graphql/resolvers/students.resolver.ts
@@ -1,9 +1,8 @@
 import { UseGuards } from "@nestjs/common";
-import { Parent, Query, ResolveField, Resolver } from "@nestjs/graphql";
+import { Parent, Query, ResolveField, Resolver, ResolveReference } from "@nestjs/graphql";
 import { EnrollmentsService } from "../../../services/enrollments.service";
 import { StudentsService } from "../../../services/students.service";
 import { AuthorizationGuard } from "../../auth/authorization.guard";
-import { AuthUser, CurrentUser } from "../../auth/current-user";
 import { Student } from "../models/student";
 
 @Resolver(() => Student)
@@ -14,12 +13,6 @@ export class StudentResolver {
     ) { }
 
     @UseGuards(AuthorizationGuard)
-    @Query(() => Student)
-    me(@CurrentUser() user: AuthUser) {
-        return this.studentsService.getStudentByAuthUserId(user.sub);
-    }
-
-    @UseGuards(AuthorizationGuard)
     @Query(() => [Student])
     students() {
         return this.studentsService.listAll();
@@ -28,5 +21,10 @@ export class StudentResolver {
     @ResolveField()
     enrollments(@Parent() students: Student) {
         return this.enrollmentsService.listByStudentId(students.id);
+    }
+
+    @ResolveReference()
+    resolveReference(reference: { authUserId: string }) {
+        return this.studentsService.getStudentByAuthUserId(reference.authUserId);
     }
 }
--- src/http/http.module.ts
+++ src/http/http.module.ts
@@ -4,7 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { DatabaseModule } from '../database/database.module';
 import path from 'node:path';
-import { ApolloDriver } from '@nestjs/apollo';
+import { ApolloFederationDriver, ApolloFederationDriverConfig } from '@nestjs/apollo';
 import { CourseResolver } from './graphql/resolvers/course.resolver';
 import { EnrollmentResolver } from './graphql/resolvers/enrollment.resolver';
 import { StudentResolver } from './graphql/resolvers/students.resolver';
@@ -16,8 +16,8 @@ import { StudentsService } from '../services/students.service';
   imports: [
     ConfigModule.forRoot(),
     DatabaseModule,
-    GraphQLModule.forRoot({
-      driver: ApolloDriver,
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
   ],
